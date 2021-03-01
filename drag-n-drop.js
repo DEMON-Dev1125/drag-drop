5130165d566e248933a2aa3f700e7569db5d765a
DEMON <729513.dev@gmail.com>
2021-03-01 02:06:51 -0800

    change coding style

--- drag-n-drop.js
+++ drag-n-drop.js
@@ -1,29 +1,28 @@
-const dragStart = event => {
-    event.currentTarget.classList.add('dragging');
+const dragStart = (event) => {
+  event.currentTarget.classList.add("dragging");
 };
-const drag = event => {
-    event.dataTransfer.setData('text/html', event.currentTarget.outerHTML);
-    event.dataTransfer.setData('text/plain', event.currentTarget.dataset.id);
+const drag = (event) => {
+  event.dataTransfer.setData("text/html", event.currentTarget.outerHTML);
+  event.dataTransfer.setData("text/plain", event.currentTarget.dataset.id);
 };
-const dragEnd = event => {
-    event.currentTarget.classList.remove('dragging');
+const dragEnd = (event) => {
+  event.currentTarget.classList.remove("dragging");
 };
 
-Array.from(document.querySelectorAll('.card')).forEach(card => {
-    card.addEventListener('dragstart', dragStart);
-    card.addEventListener('dragend', dragEnd);
-
+Array.from(document.querySelectorAll(".card")).forEach((card) => {
+  card.addEventListener("dragstart", dragStart);
+  card.addEventListener("dragend", dragEnd);
 });
 
-const dragEnter = event => {
-    event.currentTarget.classList.add('drop');
+const dragEnter = (event) => {
+  event.currentTarget.classList.add("drop");
 };
 
-const dragLeave = event => {
-    event.currentTarget.classList.remove('drop');
+const dragLeave = (event) => {
+  event.currentTarget.classList.remove("drop");
 };
 
-Array.from(document.querySelectorAll('.column')).forEach(column => {
-    column.addEventListener('dragenter', dragEnter);
-    column.addEventListener('dragleave', dragLeave);
+Array.from(document.querySelectorAll(".column")).forEach((column) => {
+  column.addEventListener("dragenter", dragEnter);
+  column.addEventListener("dragleave", dragLeave);
 });
